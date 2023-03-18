c155ba3bddf39dff85508e5278a6c266ab99f126
Elisafa <elisafajunior@gmail.com>
2023-03-18 17:02:09 -0300

    configurando o less

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,22 @@
-module.exports = function(grunt){
+module.exports = function (grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        files: {
+          'main.css': 'main.less'
+        }
+      },
+      production: {
+        options: {
+          compress: true,
+        },
+        files: {
+          'main.min.css': 'main.less'
+        }
+      }
+    }
   })
+  grunt.loadNpmTasks('grunt-contrib-less')
+  grunt.registerTask('default', ['less'])
 }
